3a55221050ea341df9b6db5badcf662531e54eee
Quantumdrill <quantumdrill317@gmail.com>
2024-10-17 20:04:51 -0400

    JSExercise

---
+++ JS/Exercise 0/main.js
@@ -0,0 +1,14 @@
+function output(val){
+    for(let i = 0; i<val;i++){
+        display.innerHTML+=`We got: ${(i+1)**2}<br>`;
+    }
+}
+
+inpt.oninput = function(){
+    display.innerHTML="";
+    if (inpt.value>500){
+        window.alert("NO");
+    } else {
+        output(inpt.value);
+    }
+}

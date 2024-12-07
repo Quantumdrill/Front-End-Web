f69955f6df2b84d0a26109bb8c3b1d2199668f74
Quantumdrill <quantumdrill317@gmail.com>
2024-12-07 05:21:59 -0500

    Final.v2

---
+++ Final/home.js
@@ -0,0 +1,14 @@
+anime({
+    targets: "#Line",
+    width: ["0vw", "92vw"],
+    duration: 1000,
+    easing: "easeInOutQuad",
+})
+
+anime({
+    targets: "#Title",
+    left: ["100vw", "8vw"],
+    duration: 1000,
+    easing: "easeInOutQuad",
+    delay: 100,
+})
